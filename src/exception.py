91c89bd04cfacfcea2997e87f113cff7da0dce0f
poojapbabar <babarpooja2002@gmail.com>
2024-04-17 22:28:22 +0530

    eda and model trai

--- src/exception.py
+++ src/exception.py
@@ -21,12 +21,12 @@ class CustomException(Exception):
         return self.error_message 
     
 
-    if __name__ =="__main__":
+    # if __name__ =="__main__":
 
 
-        try:
-            a = 1/0
-        except Exception as e:
-            logging.info("Divide by zero")
-        except Exception as e:
-            raise CustomException(e,sys)
+    #     try:
+    #         a = 1/0
+    #     except Exception as e:
+    #         logging.info("Divide by zero")
+    #     except Exception as e:
+    #         raise CustomException(e,sys)
